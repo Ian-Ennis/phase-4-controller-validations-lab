7480b853612529e4b08e71877b0717960061cb61
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-01-11 11:35:01 -0700

    commit tree ee614f6a794701808da946050424bdaf42f84067

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,12 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
     post.update(post_params)
-
-    render json: post
+    if post.valid?
+    render json: post, status: :created
+    else 
+      render json: {errors: post.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   private
